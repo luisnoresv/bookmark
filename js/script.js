bee00167ca69f9eb679aabe4411088fc45b187cf
Luis Alberto Nores Villaverde <luisnoresv@outlook.com>
2022-12-21 12:17:55 -0500

    Design and main features finish

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const tabs = document.querySelectorAll('.tab');
+const panels = document.querySelectorAll('.panel');
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+const logo = document.getElementById('logo');
+
+// Tabs menu event listener
+function onTabClick(e) {
+	// Deactivate all tabls
+	tabs.forEach((tab) => {
+		tab.children[0].classList.remove(
+			'border-softRed',
+			'border-b-4',
+			'md:border-b-0'
+		);
+	});
+
+	// Hide all panels
+	panels.forEach((panel) => panel.classList.add('hidden'));
+
+	// Activate a new tab and panel based on the target
+	e.target.classList.add('border-softRed', 'border-b-4');
+	const classString = e.target.getAttribute('data-target');
+	document
+		.getElementById('panels')
+		.getElementsByClassName(classString)[0]
+		.classList.remove('hidden');
+}
+
+tabs.forEach((tab) => {
+	tab.addEventListener('click', onTabClick);
+});
+
+// Hamburger button listener
+
+function navToggle() {
+	menuBtn.classList.toggle('open');
+	menu.classList.toggle('flex');
+	menu.classList.toggle('hidden');
+
+	if (menu.classList.contains('flex')) {
+		logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+	} else {
+		logo.setAttribute('src', './images/logo-bookmark.svg');
+	}
+}
+
+menuBtn.addEventListener('click', navToggle);
